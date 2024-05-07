f23e6b6b37ae5d24611511b4f646510e3387bb99
KiteOfficial <sirdewong@gmail.com>
2024-05-07 13:38:02 +0800

    user types

--- src/lib/states/userState.ts
+++ src/lib/states/userState.ts
@@ -1,16 +1,32 @@
-import { observable } from "@legendapp/state";
+import {  ObservableObject,  computed,  observable } from "@legendapp/state";
 import { configureObservablePersistence, persistObservable } from '@legendapp/state/persist'
 import { ObservablePersistLocalStorage } from '@legendapp/state/persist-plugins/local-storage'
+import { decodeToken } from "react-jwt";
 
 configureObservablePersistence({
   pluginLocal: ObservablePersistLocalStorage
 })
 
 
-const user$ = observable({
-  user: {},
+const user$: ObservableObject<{
+  isLoggedIn: boolean,
+  token: {
+    token: string,
+    user_id: number,
+  },
+  user: {
+    id: number,
+    username: string,
+    iat: number,
+    exp: number,
+  } | unknown
+}> = observable({
   isLoggedIn: false,
-  token: ""
+  token: {
+    token: "",
+    user_id: 0,
+  },
+  user: {}
 });
 
 persistObservable(user$, {
